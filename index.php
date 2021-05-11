d9aeb4d8330cb4d94622ad8150a50d1c123e192c
Frederic <fredericdurand972@gmail.com>
2021-05-11 15:27:19 +0200

    css, html

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>conflit</title>
+</head>
+<body>
+    <h1>conflit</h1>
+    <p></p>
+    <h2></h2>
+    <p></p>
+    
+</body>
+</html>
